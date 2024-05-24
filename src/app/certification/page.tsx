382d723fe91b1810282c0eb4f8614e1f3c7a375f
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-05-24 13:12:02 +0700

    tiny adjustment

--- src/app/certification/page.tsx
+++ src/app/certification/page.tsx
@@ -4,7 +4,7 @@ import { FlexCenterY } from "../ui/main/style";
 export default function page() {
   return (
     <section id="certification" className={`tw-bg-secondary-light tw-min-h-screen ${FlexCenterY} tw-flex-row tw-justify-around tw-flex-wrap tw-p-10 tw-gap-5 tw-relative`}>
-      <h2 className="tw-font-black tw-text-4xl md:tw-text-8xl !tw-mb-10 tw-w-full tw-z-10">Certification</h2>
+      <h2 className="tw-font-black tw-text-4xl md:tw-text-8xl !tw-mb-10 tw-w-full tw-z-10 tw-text-center md:tw-text-left">Certification</h2>
       {Certification?.map((item, index) => (
         <div key={index} className="tw-w-full md:tw-w-5/12 tw-z-10">
           <img src={item.image} alt={item.title} className="tw-w-full tw-border tw-border-solid tw-border-dark" />
--- src/app/ui/main/backToHomeButton.tsx
+++ src/app/ui/main/backToHomeButton.tsx
@@ -9,7 +9,7 @@ export default function BackToHomeButton() {
   const pathname = usePathname();
 
   return (
-    <motion.div layout animate={{ left: `${pathname !== "/" ? 0 : '-6rem'}` }} style={{ left: '-6rem' }} className='tw-fixed tw-p-3 tw-top-5 tw-z-50'>
+    <motion.div layout animate={{ left: `${pathname !== "/" ? 0 : '-6rem'}` }} style={{ left: '-6rem' }} className='tw-fixed tw-p-3 tw-top-0 tw-z-50'>
       <ButtonLinkPrimary link='/#list-of-content' icon={<FaArrowLeft />} shape='circle' floating />
     </motion.div>
   )
