39218acc434bfcdc66732f991e2375617f6e0646
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-05-24 15:41:29 +0700

    add arrow in list of contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { FlexCenter, FlexCenterX } from "./ui/main/style";
+import { FaArrowRight, FaUser } from "react-icons/fa6";
+import { FlexCenter, FlexCenterX, FlexCenterY } from "./ui/main/style";
 
 export default function Home() {
   return (
@@ -21,13 +22,13 @@ export default function Home() {
 			<section id="list-of-content" className={`tw-bg-secondary-light tw-min-h-screen tw-flex tw-items-center tw-flex-row tw-justify-between tw-flex-wrap tw-relative`}>
 				<h2 className="tw-text-5xl md:tw-text-8xl tw-font-black tw-w-full md:tw-w-6/12 !tw-pl-20 !tw-pt-20 !tw-pb-5">List Of Contents</h2>
 				<div className={`${FlexCenterX} tw-flex-col tw-bg-primary tw-w-full md:tw-w-6/12 tw-text-2xl tw-font-bold md:tw-text-4xl md:tw-min-h-screen tw-px-10 tw-pb-20 md:tw-pb-0`}>
-					<a href="#cover" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">01 Cover</a>
-					<a href="#list-of-content" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">02 List Of Contents</a>
-					<a href="/about-me" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">03 About Me</a>
-					<a href="/education" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">04 Education</a>
-					<a href="/certification" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">05 Certification</a>
-					<a href="/project-portfolio" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">06 Project Portfolio</a>
-					<a href="/contact" className="!tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent">07 Contact</a>
+					<a href="#cover" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>01 Cover <FaArrowRight /></a>
+					<a href="#list-of-content" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>02 List Of Contents <FaArrowRight /></a>
+					<a href="/about-me" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>03 About Me <FaArrowRight /></a>
+					<a href="/education" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>04 Education <FaArrowRight /></a>
+					<a href="/certification" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>05 Certification <FaArrowRight /></a>
+					<a href="/project-portfolio" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>06 Project Portfolio <FaArrowRight /></a>
+					<a href="/contact" className={`${FlexCenterY} !tw-my-5 tw-no-underline tw-text-dark hover:tw-text-accent tw-gap-3`}>07 Contact <FaArrowRight /></a>
 				</div>
 				<div className="tw-absolute tw-font-bold tw-text-xl tw-bottom-10 tw-right-10 md:tw-bottom-16 md:tw-right-16">{new Date().getFullYear()}</div>
         <div className={`${FlexCenter} tw-border-t tw-border-r-0 tw-border-l tw-border-b-0 tw-border-dark tw-border-solid tw-w-16 tw-h-16 md:tw-w-44 md:tw-h-44 tw-absolute tw-left-5 tw-top-5`}>
