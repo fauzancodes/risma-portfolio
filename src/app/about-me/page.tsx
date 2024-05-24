c5ea73770dec0fe0d70bcda11e21df1a7d39c8ab
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-05-24 20:19:59 +0700

    add animation in about me

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,25 +1,18 @@
-import { About, ProfilePhoto } from "@/constants";
+import { About } from "@/constants";
 import { FlexCenter } from "../ui/main/style";
-import { allura } from "../ui/main/fonts";
+import { Description, Header, ProfileImage } from "../ui/animated/about-me";
 
 export default function page() {
   return (
     <section id="about-me" className={`tw-bg-secondary-light tw-min-h-screen tw-flex tw-flex-row tw-justify-between tw-flex-wrap`}>
       <div className={`${FlexCenter} tw-w-full md:tw-w-[45%] tw-min-h-[30rem] md:tw-min-h-screen tw-my-5 md:tw-my-0`}>
         <div className={`${FlexCenter} tw-w-full tw-h-full md:tw-h-screen tw-border-t tw-border-l tw-border-r-0 tw-border-b-0 tw-border-dark tw-border-solid tw-m-5 md:tw-mr-0 tw-p-5`}>
-          <div className={`${FlexCenter} tw-border tw-border-dark tw-border-solid tw-h-full tw-w-full tw-relative`}>
-            <img src={ProfilePhoto} alt="Profile Image" className="tw-w-full tw-absolute tw-bottom-0" />
-          </div>
+          <ProfileImage />
         </div>
       </div>
-      <div className={`tw-bg-primary tw-px-10 tw-py-12 md:tw-w-[55%]`}>
-        <h2 className={`${allura.className} tw-text-6xl md:tw-text-9xl`}>Introducing</h2>
-        <h2 className="tw-font-black tw-text-5xl md:tw-text-8xl tw-text-right !tw-mb-10">About Me</h2>
-        {About?.map((item, index) => (
-          <p key={index} className="!tw-my-5 md:tw-text-xl">
-            {item}
-          </p>
-        ))}
+      <div className={`tw-bg-primary tw-px-10 tw-py-12 md:tw-w-[55%] tw-overflow-x-hidden`}>
+        <Header />
+        <Description />
       </div>
     </section>
   )
---
+++ src/app/ui/animated/about-me.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { About, ProfilePhoto } from "@/constants"
+import { FlexCenter } from "../main/style"
+import { motion } from 'framer-motion'
+import { allura } from "../main/fonts"
+
+export function ProfileImage() {
+  return (
+    <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} className={`${FlexCenter} tw-border tw-border-dark tw-border-solid tw-h-full tw-w-full tw-relative`}>
+      <img src={ProfilePhoto} alt="Profile Image" className="tw-w-full tw-absolute tw-bottom-0" />
+    </motion.div>
+  )
+}
+
+export function Header() {
+  return (
+    <>
+      <motion.h2 animate={{ translateX: 0, opacity: 1 }} initial={{ translateX: "-2rem", opacity: 0 }} className={`${allura.className} tw-text-6xl md:tw-text-9xl`}>Introducing</motion.h2>
+      <motion.h2 animate={{ translateX: 0, opacity: 1 }} initial={{ translateX: "2rem", opacity: 0 }} className="tw-font-black tw-text-5xl md:tw-text-8xl tw-text-right !tw-mb-10">About Me</motion.h2>
+    </>
+  )
+}
+
+export function Description() {
+  return (
+    <>
+      {About?.map((item, index) => (
+        <motion.p key={index} animate={{ opacity: 1 }} initial={{ opacity: 0 }} className="!tw-my-5 md:tw-text-xl">
+          {item}
+        </motion.p>
+      ))}
+    </>
+  )
+}
