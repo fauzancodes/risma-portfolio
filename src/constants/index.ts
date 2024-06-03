7b97ebf04a548b213f741d794e8668edd88559a1
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-06-03 17:48:31 +0700

    refined English in portfolio

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -50,19 +50,19 @@ export const WorkExperiences = [
   {
     title: "PT Krida Hangulindo Utama",
     duration: "2020 until 2024",
-    description: "I have experience working at PT Krida Hangulindo Utama from August 18, 2020, to January 31, 2024, holding the position as a Project Administrator and a Purchasing Administrator. The Company operates in the construction/pipe installation field for wastewater treatment (WWTP/WTP), located in East Cikarang, West Java.",
+    description: "I have experience working at PT Krida Hangulindo Utama from August 18, 2020, to January 31, 2024, holding the position as a Project Administrator and a Purchasing Administrator. This company is engaged in the construction/installation of pipes for waste water treatment (IPAL/WTP), located in East Cikarang, West Java.",
     jobdesks: [
-      "Creating Daily Reports, Weekly Reports and Monthly Project Reports",
-      "Creating Purchase Requests and Purchase Orders for Materials and Consumables",
-      "Compiling a Data List of Items and Prices for Materials, Consumbales, and Media",
-      "Contacting and Negotiating with Suppliers for Purchasing Goods",
-      "Contacting Customers Regarding Fieldwork",
-      "Generating Material, Consumable, and Tool Delivery Notes",
-      "Creating Manpower Work Schedules",
-      "Generating Monthly Reports on Material, Tool and Consumable Purchases",
-      "Creating Daily Project Overtime Forms",
-      "Drafting Project Completion Certificates",
-      "Providing Quotations for Tools/Materials to Customers"
+      "Create Daily Reports, Weekly Reports and Monthly Project Reports",
+      "Create Purchase Requisitions and Purchase Orders for Materials and Consumables",
+      "Compile a list of data on goods and prices for materials, consumables and media",
+      "Contacting and Negotiating with Suppliers to Purchase Goods",
+      "Contacting Customers About Work in the Field",
+      "Create Delivery Notes for Materials, Consumables, and Tools",
+      "Create Manpower Work Schedules",
+      "Create Monthly Reports on Purchases of Materials, Tools and Consumables",
+      "Create a Daily Project Overtime Forms",
+      "Create Project Completion Minutes",
+      "Providing Tools/Materials Offers to Customers"
     ],
     images: [
       "/images/WhatsApp Image 2024-05-12 at 15.01.43.webp",
@@ -73,10 +73,10 @@ export const WorkExperiences = [
         title: "Reporting Document",
         subtitle: "Daily, Weekly, Monthly",
         description: [
-          "The purpose of reporting document is to track the progress of work carried out by manpower each day.",
-          " These reports entail daily reports (yesterday's work outcomes and today's work plan), weekly reports covering a one-week period, and monthly reports covering a one-month period.",
-          "For daily and weekly reports, they will be provided to supervisors, the head of the engineering division, and the project manager. Meanwhile, monthly reports will be delivered to the technical director and the client.",
-          "All these reports are compiled using Microsoft Excel and Microsoft Word."
+          "The purpose of reporting document is to track the progress of work carried out by manpower every day.",
+          "These reports include daily reports (yesterday's work results and today's work plan), weekly reports for a period of one week, and monthly reports for a period of one month.",
+          "Daily and weekly reports will be provided to supervisors, heads of engineering divisions, and project managers. Meanwhile, monthly reports will be submitted to the technical director and clients.",
+          "All these reports were prepared using Microsoft Excel and Microsoft Word."
         ],
         extra: [],
         images: [
@@ -102,11 +102,11 @@ export const WorkExperiences = [
         ],
         extra: [
           "Client's request for specific goods/services",
-          "Compilation of a list of required items along with their prices",
-          "Preparation of price quotation documents for the requested items",
-          "Upon acceptance of the quotation and client's agreement to purchase from us, they send a Purchase Order (P.O) for the items",
-          "Purchase order processing with the supplier",
-          "Goods ready for distribution to the client"
+          "Make a data list regarding what items will be needed along with the price of the items",
+          "Create a price offer document for the goods requested by the client",
+          "Once the offer is accepted and the client agrees to purchase the item from us, they send us a P.O for the item",
+          "Purchase order process to supplier",
+          "Goods are ready to be distributed to clients"
         ],
         images: [
           {
@@ -126,8 +126,8 @@ export const WorkExperiences = [
         title: "Manpower Monitoring",
         subtitle: "Documentation / Schedule",
         description: [
-          "Before the project commencement, as the project administrator, I will hold discussions/meetings with the team leader to develop a work  schedule from start to finish. The purpose of creating this schedule is to monitor the progress of work conducted by the manpower to ensure it aligns with the timeline. Additionally, it serves as a reference for the workers to perform their tasks according to procedures.",
-          "Once the work schedule is established, every day as the project administrator , I will conduct monitoring either in the workshop or in the field. This monitoring involves documenting photos of completing checklists based on the scheduled tasks. These photos will be used later in daily, weekly, and monthly reports."
+          "Before the project starts,, as the project administrator, I will hold discussions/meetings with the team leader to develop a work  schedule from start to finish. The purpose of creating this schedule is to monitor the progress of work conducted by the manpower to ensure it aligns with the timeline. Additionally, it serves as a reference for the workers to perform their tasks according to procedures.",
+          "After the work schedule has been prepared, every day as the project administrator , I will conduct monitoring either in the workshop or in the field. This monitoring involves documenting photos of completing checklists based on the scheduled tasks. These photos will be used later in daily, weekly, and monthly reports."
         ],
         extra: [],
         images: [
@@ -173,114 +173,6 @@ export const WorkExperiences = [
             image: "/images/WhatsApp Image 2024-05-14 at 16.05.13.webp"
           }
         ]
-      }
-    ]
-  }
-]
-
-export const WorkPortfolio = [
-  {
-    title: "Reporting Document",
-    subtitle: "Daily, Weekly, Monthly",
-    description: [
-      "The purpose of reporting document is to track the progress of work carried out by manpower each day.",
-      " These reports entail daily reports (yesterday's work outcomes and today's work plan), weekly reports covering a one-week period, and monthly reports covering a one-month period.",
-      "For daily and weekly reports, they will be provided to supervisors, the head of the engineering division, and the project manager. Meanwhile, monthly reports will be delivered to the technical director and the client.",
-      "All these reports are compiled using Microsoft Excel and Microsoft Word."
-    ],
-    extra: [],
-    images: [
-      {
-        title: "Daily Report Example",
-        image: "/images/image (3).webp"
-      },
-      {
-        title: "Weekly Report Example",
-        image: "/images/image (2).webp"
-      },
-      {
-        title: "Monthly Report Example",
-        image: "/images/image (1)_waifu2x_art_scan_noise3_scale.webp"
-      }
-    ]
-  },{
-    title: "Purchasing Activity",
-    subtitle: "Quotation, Material Data List, P.O",
-    description: [
-      "The purchasing division is a part of the company that responsible for providing goods/services for the operational needs of the company. In my previous workplace, there were 6 stages in the procurment process.",
-      "In the purchasing process, the ability to negotiate to obtain the best goods at the best price and with the best service is crucial in order to generate greater profits for the company."
-    ],
-    extra: [
-      "Client's request for specific goods/services",
-      "Compilation of a list of required items along with their prices",
-      "Preparation of price quotation documents for the requested items",
-      "Upon acceptance of the quotation and client's agreement to purchase from us, they send a Purchase Order (P.O) for the items",
-      "Purchase order processing with the supplier",
-      "Goods ready for distribution to the client"
-    ],
-    images: [
-      {
-        title: "Quotation Example",
-        image: "/images/test-images-0.webp"
-      },
-      {
-        title: "Material Data List Example",
-        image: "/images/image.webp"
-      },
-      {
-        title: "Purchase Order Example",
-        image: "/images/PO FILTER PRESS & DISC FILTER - BIO TONIC-images-0.webp"
-      }
-    ]
-  },{
-    title: "Manpower Monitoring",
-    subtitle: "Documentation/Schedule",
-    description: [
-      "Before the project commencement, as the project administrator, I will hold discussions/meetings with the team leader to develop a work  schedule from start to finish. The purpose of creating this schedule is to monitor the progress of work conducted by the manpower to ensure it aligns with the timeline. Additionally, it serves as a reference for the workers to perform their tasks according to procedures.",
-      "Once the work schedule is established, every day as the project administrator , I will conduct monitoring either in the workshop or in the field. This monitoring involves documenting photos of completing checklists based on the scheduled tasks. These photos will be used later in daily, weekly, and monthly reports."
-    ],
-    extra: [],
-    images: [
-      {
-        title: "Workshop Documentation Example",
-        image: "/images/IMG_20201120_141450.webp"
-      },
-      {
-        title: "On Site Documentation Example",
-        image: "/images/WhatsApp Image 2024-05-14 at 15.11.08.webp"
-      },
-      {
-        title: "Work Schedule Example",
-        image: "/images/WWTP Schedule Plan-images-0.webp"
-      }
-    ]
-  },{
-    title: "Materials Controlling",
-    subtitle: "Checking & Delivering Notes",
-    description: [
-      "As a project administrator, besides being responsible for creating reports and monitoring manpower, I am also tasked with checking incoming and outgoing items, including materials, consumables, tools, and machines. Incoming items are recorded in the incoming goods log. For outgoing items, a delivery note is usually created. This delivery note includes the following information:",
-      "The information about incoming and outgoing items is not only recorded in the logbook but is also usually shared in the office WhatsApp group as evidence."
-    ],
-    extra: [
-      "Delivery Date",
-      "Document Number",
-      "Vehicle Type & License Plate Number",
-      "Quantity, item Name, & Specifications",
-      "Signature of the Responsible Person along with the Stamp",
-      "Stamp & Signature of Security"
-    ],
-    images: [
-      {
-        title: "Incoming Item Info Example",
-        image: "/images/WhatsApp Image 2024-05-14 at 16.12.38.webp"
-      },
-      {
-        title: "Outgoing Item Info Example",
-        image: "/images/WhatsApp Image 2024-05-14 at 16.12.38 (1).webp"
-      },
-      {
-        title: "Delivery Note Example",
-        image: "/images/WhatsApp Image 2024-05-14 at 16.05.13.webp"
       }
     ]
   }
