6e9e3cb3eea69e24e39dad844cfb626d62e19b3d
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-07-06 21:35:02 +0700

    change download cv cta

--- src/app/ui/main/cvButton.tsx
+++ src/app/ui/main/cvButton.tsx
@@ -3,7 +3,7 @@ import { ButtonLinkPrimary } from "./button"
 const CvButton = () => {
   return (
     <div className='tw-fixed tw-bottom-5 tw-left-5 tw-z-50'>
-      <ButtonLinkPrimary link='/CV - Rismawati.pdf' floating text='Download CV' download />
+      <ButtonLinkPrimary link='/CV - Rismawati.pdf' floating text='Curriculum Vitae - Rismawati' download />
     </div>
   )
 }
